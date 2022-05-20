f3e99d410424d1a7f5c9c905579cae7605537b03
Sander Ruys <ruyssander@gmail.com>
2022-05-20 16:44:56 +0200

    Check if sum is above 21

--- 2_21-Card-game/assets/script.js
+++ 2_21-Card-game/assets/script.js
@@ -4,13 +4,18 @@ and an array for the Suits: Spades, Diamonds, Clubs, Hearts
 After you made those arrays you need an empty array for the deck where you can push the cards to
 All the values need to be assigned to each suit so you get 52cards in total in  one deck*/
 
+const startGameButton = document.getElementById("play");
+const hitButton = document.getElementById("pick-card");
 const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-let deck = new Array();
+const deck = [];
+let playersHand = [];
+let dealersHand = [];
+let playerPoints = 0;
+let dealerPoints = 0;
 
 //make your full card deck
-function createDeck(){
-    deck = new Array();
+const  createDeck = () => {
     for (let i = 0 ; i < values.length; i++)
     {
         for(let x = 0; x < suits.length; x++)
@@ -24,26 +29,60 @@ function createDeck(){
             deck.push(card);
         }
     }
+    console.log(deck);
 }
-createDeck();
-console.log("nr of cards = " + deck.length);
 
 //shuffle the cards
-function shuffle(){
+const shuffle = () => {
     // for 1000 turns
     // switch the values of two random cards
-    for (var i = 0; i < 1000; i++)
+    for (let i = 0; i < 1000; i++)
     {
-        var location1 = Math.floor((Math.random() * deck.length));
-        var location2 = Math.floor((Math.random() * deck.length));
-        var tmp = deck[location1];
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let tmp = deck[location1];
 
         deck[location1] = deck[location2];
         deck[location2] = tmp;
     }
 }
 
+const dealFirstHand = () =>{
+    for (let i = 0; i < 2; i++) {
+        giveCardToPlayer();
+        giveCardToDealer();
+        
+    }
+}
+
+const giveCardToPlayer = () =>{
+    const firstCard = deck.pop();
+    playersHand.push(firstCard);
+    console.log(playersHand);
+
+    //check if card  > 21
+    playerPoints = playersHand.map(item => item.Weight).reduce((prev, curr) => prev + curr, 0);
+    console.log("playerPoints = " + playerPoints);
+
+}
+const giveCardToDealer = () =>{
+    const firstCard = deck.pop();
+    dealersHand.push(firstCard);
+    //check if card  > 21
+}
+
 //start the game
-function startTheGame() {
+const startTheGame = () => {
+    createDeck();
+    shuffle();
+    //deal first hand
+    dealFirstHand();
+    //
+    console.log(playersHand);
     
 }
+
+
+//button click eventListeners
+startGameButton.addEventListener("click", startTheGame);
+hitButton.addEventListener("click", giveCardToPlayer);
