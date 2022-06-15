f7eef19870dc4d86de221446ac21d653b2e817ef
Sander Ruys <ruyssander@gmail.com>
2022-06-15 12:24:33 +0200

    Add image grid

---
+++ 3_Memory-game/scripts/cards.js
@@ -0,0 +1,21 @@
+// put cars in an array
+
+const cardsArray = [
+  {
+    name: "aerodactyl",
+    img: "../images/aerodactyl.jpg",
+    alt: "image of aerodactyl",
+  },
+  {
+    name: "articuno",
+    img: "../images/articuno.jpg",
+    alt: "image of articuno",
+  },
+  {
+    name: "blastoise",
+    img: "../images/blastoise.jpg",
+    alt: "image of blastoise",
+  },
+];
+
+export { cardsArray };
---
+++ 3_Memory-game/scripts/index.js
@@ -0,0 +1,17 @@
+import { cardsArray } from "./cards.js";
+
+/*const main = document.querySelector("main");
+
+// display card function
+const displayCard = (name, img, alt) =>{
+    let card = document.createElement("img");
+    card.src = img;
+    card.alt = alt;
+    card.addEventListener("click", alert(name + "geclicked"));
+    main.append(card);
+}
+
+for (let i = 0; i < cardsArray.length; i++) {
+    displayCard(cardsArray[i].img, cardsArray[i].alt );
+    
+}*/
